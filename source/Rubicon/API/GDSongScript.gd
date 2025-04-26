6a7a12912c28d0a10134e6b680b6533e5d289319
Binpuki <30415792+Binpuki@users.noreply.github.com>
2025-04-26 16:15:58 -0500

    GDScript song script coroutines

--- source/Rubicon/API/GDSongScript.gd
+++ source/Rubicon/API/GDSongScript.gd
@@ -17,3 +17,21 @@ func step_hit(_step : int) -> void:
 	
 func note_hit(_bar_line_name : StringName, _result : NoteResult):
 	pass
+
+func wait_for_second(time : float) -> void:
+	if Conductor.Time >= time:
+		return
+	
+	await get_tree().process_frame
+
+func wait_for_measure(measure : float) -> void:
+	while Conductor.GetCurrentMeasure() < measure:
+		await get_tree().process_frame
+	
+func wait_for_beat(beat : float) -> void: 
+	while Conductor.GetCurrentBeat() < beat:
+		await get_tree().process_frame
+
+func wait_for_steps(step : float) -> void:
+	while Conductor.GetCurrentStep() < step:
+		await get_tree().process_frame	
